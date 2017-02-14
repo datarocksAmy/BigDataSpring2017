4ebd8620c6c908381b3610202f3865b25013269d
datarocksAmy <clthf@mail.umkc.edu>
2017-02-14 10:36:21 -0600

    Lab Update

---
+++ BigData-Lab Assignment #4/Spark Programming Lab #4 - Image Classification/data/src/main/scala/IPSettings.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by pradyumnad on 19/07/15.
+  */
+
+// Every setting for the ImageUtils
+object IPSettings {
+
+  val PATH = "data/"
+  val INPUT_DIR = PATH + "traindata"
+  val TEST_INPUT_DIR = PATH + "TestData"
+  val FEATURES_PATH = PATH + "model/features"
+
+  val KMEANS_PATH = PATH + "model/clusters"
+  val KMEANS_CENTERS_PATH = PATH + "model/clusterCenters"
+  val HISTOGRAM_PATH = PATH + "model/histograms"
+  val RANDOM_FOREST_PATH = PATH + "model/nbmodel"
+}
