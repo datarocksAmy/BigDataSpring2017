5d2fc0e016b094a7dc397ade1ce64b334e5d0015
datarocksAmy <clthf@mail.umkc.edu>
2017-02-22 22:06:39 -0600

    Lab Update

---
+++ BigData-Lab Assignment #5/Client Application using Spark API/src/main/scala/IPApp.scala
@@ -0,0 +1,58 @@
+/**
+  * =====================================================================
+  * CS5542 Big Data Analytics & ApplicationLab
+  * Assignment #5 - Client Application using Spark API
+  * Using JetBrains Extension on Google to connect the Spark Program for image recognition.
+  * User defined dataset. --- bubble tea, chocolate, coffee, mocha tea
+  * #20 Chia-Hui Amy Lin
+  * =====================================================================
+  */
+
+
+import java.nio.file.{Files, Paths}
+
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.tree.model.RandomForestModel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object IPApp {
+  val IMAGE_CATEGORIES = List("bubbleTea", "chocolate", "coffee", "mochaTea")
+
+  /**
+    * @note Test method for classification on Spark
+    * @param sc : Spark Context
+    * @return
+    */
+  def testImageClassification(sc: SparkContext, path: String): String ={
+
+    val model = KMeansModel.load(sc, IPSettings.KMEANS_PATH)
+    val vocabulary = ImageUtils.vectorsToMat(model.clusterCenters)
+    val desc = ImageUtils.bowDescriptors(path, vocabulary)
+    val histogram = ImageUtils.matToVector(desc)
+
+    println("-- Histogram size : " + histogram.size)
+    println(histogram.toArray.mkString(" "))
+
+    val nbModel = RandomForestModel.load(sc, IPSettings.RANDOM_FOREST_PATH)
+    val p = nbModel.predict(histogram)
+    (s"Test image predicted as : " + IMAGE_CATEGORIES(p.toInt))
+  }
+
+
+  def testImage(string: String):String = {
+    val conf = new SparkConf()
+      .setAppName(s"IPApp")
+      .setMaster("local[*]")
+      .set("spark.executor.memory", "6g")
+      .set("spark.driver.memory", "6g")
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+    val sc= SparkContext.getOrCreate(sparkConf)
+    val res = testImageClassification(sc, string)
+
+    printf(res);
+    res
+  }
+}
