ec949b664104b246a26e310e35aa548be6131e28
datarocksAmy <clthf@mail.umkc.edu>
2017-03-01 16:43:19 -0600

    Lab Update

---
+++ BigData-Lab Assignment #6/Spark Programming Lab #6 - Image Classification+Object Detection/Feature Extraction/src/main/java/ObjectFeatureExtraction.java
@@ -0,0 +1,40 @@
+import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.feature.local.engine.DoGSIFTEngine;
+import org.openimaj.image.feature.local.keypoints.Keypoint;
+
+import java.io.*;
+
+/**
+ * =====================================================================
+ * CS5542 Big Data Analytics & Application Lab
+ * Assignment #6 - Object Feature Extraction
+ * Obtain Feature Extraction for each image category seperately
+ * #20 Chia-Hui Amy Lin
+ * =====================================================================
+ */
+public class ObjectFeatureExtraction {
+    public static void main(String args[]) throws IOException {
+        String inputFolder = "data/";
+        String inputImage = "chameleon2.jpg";  // Manually doing to test the images; What kind of image details/features you want to get
+        String outputFolder = "output/";
+        String[] IMAGE_CATEGORIES = {"Sea", "PolarBear", "Fish", "Peacock", "Chameleon"};
+
+        int input_class = 4; // Switch the order in the previous list
+        MBFImage mbfImage = ImageUtilities.readMBF(new File(inputFolder + inputImage));
+        DoGSIFTEngine doGSIFTEngine = new DoGSIFTEngine();
+        LocalFeatureList<Keypoint> features = doGSIFTEngine.findFeatures(mbfImage.flatten());
+        FileWriter fw = new FileWriter(outputFolder + IMAGE_CATEGORIES[input_class] + ".txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = 0; i < features.size(); i++) {
+            double c[] = features.get(i).getFeatureVector().asDoubleVector();
+            bw.write(input_class + ",");
+            for (int j = 0; j < c.length; j++) {
+                bw.write(c[j] + " ");
+            }
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
